3dd4335bccab5471139b3b9e8a6d199b1ca5ba6c
jakesutor <jake.sutor@gmail.com>
2020-08-12 19:53:04 -0400

    Add files via upload

--- Data Cleansing/Cancelled Flights Cleansing.py
+++ Data Cleansing/Cancelled Flights Cleansing.py
@@ -1,10 +1,11 @@
 # -*- coding: utf-8 -*-
 """
-Created on Sun Jun 28 15:43:17 2020
+Created on Fri Jul  3 21:07:56 2020
 
 @author: jakes
 """
 
+#Import necessary libraries
 import pandas as pd
 import numpy as np
 import datetime as dt
@@ -16,10 +17,35 @@ from tqdm import tqdm
 import os
 os.chdir(r'C:\Users\jakes\Downloads')
 
+#Read in dataset for all flights
+#df = pd.read_csv('On_Time_Marketing_Carrier_On_Time_Performance_(Beginning_January_2018)_2019_10.csv')  
+df = pd.read_csv('data.csv',encoding= 'unicode_escape',error_bad_lines=False)
+#Separate out only flights that were considered "Delayed" by the BTS
+df2 = df[df["ARR_DELAY_NEW"]>=15]
+
+#Extract the airport codes and arrival delays for all delayed flights in the dataset
+airports = df2[["DEST","ARR_DELAY_NEW"]]
+
+#Calculate the mean arrival delay of delayed flights for each unique airport
+means=airports.groupby('DEST',as_index=False)['ARR_DELAY_NEW'].mean()
+#Round the mean arrival delay to the nearest integer to match 
+means.ARR_DELAY_NEW=means.ARR_DELAY_NEW.astype(float).round()
+means.head()
+
 #read csv files for all cancelled flights
 data = pd.read_csv('cancelled_raw_data.csv')
 data.head()
 data = data[['FL_DATE','MKT_UNIQUE_CARRIER','ORIGIN','ORIGIN_CITY_NAME','DEST','DEST_CITY_NAME','CRS_DEP_TIME', 'CRS_ARR_TIME','CANCELLED']]
+
+
+#Fill in the missing arrival delay values for the cancelled flights
+# Create empty attribute ARR_DELAY_NEW
+data['ARR_DELAY_NEW'] = 0
+data['ARR_DELAY_NEW']=data['DEST'].map(dict(zip(means['DEST'],means['ARR_DELAY_NEW']))).fillna(data.ARR_DELAY_NEW)
+#Verify it filled in correctly
+data.ARR_DELAY_NEW
+
+data.head()
 
 
 # Use the same methodology to change time to EST
@@ -129,15 +155,15 @@ data['CRS_DEP_DATE'] = data.progress_apply(get_date_crs_dep,axis=1)
 data['CRS_ARR_DATE'] = data.progress_apply(get_date_crs_arr,axis=1)
 
 # Create dataframe with the desired column names
-data = data[['CRS_DEP_DATETIME_EST','CRS_DEP_DATE','CRS_DEP_HOUR','CRS_ARR_DATETIME_EST','CRS_ARR_DATE','CRS_ARR_HOUR','MKT_UNIQUE_CARRIER','ORIGIN','ORIGIN_CITY_NAME','DEST','DEST_CITY_NAME','CANCELLED']]
-data.columns = ['CRS_DEP_DATETIME_EST','CRS_DEP_DATE','CRS_DEP_HOUR','CRS_ARR_DATETIME_EST','CRS_ARR_DATE','CRS_ARR_HOUR','Marketing_Airline_Network','Origin','OriginCityName','Dest','DestCityName','CANCELLED']
+data = data[['CRS_DEP_DATETIME_EST','CRS_DEP_DATE','CRS_DEP_HOUR','CRS_ARR_DATETIME_EST','CRS_ARR_DATE','CRS_ARR_HOUR','MKT_UNIQUE_CARRIER','ORIGIN','ORIGIN_CITY_NAME','DEST','DEST_CITY_NAME','CANCELLED','ARR_DELAY_NEW']]
+data.columns = ['CRS_DEP_DATETIME_EST','CRS_DEP_DATE','CRS_DEP_HOUR','CRS_ARR_DATETIME_EST','CRS_ARR_DATE','CRS_ARR_HOUR','Marketing_Airline_Network','Origin','OriginCityName','Dest','DestCityName','CANCELLED','ArrDelayMinutes']
 
 # Create csv from dataframe
 data.to_csv('cleaned_cancelled_data.csv')
 
 # Divide into departure and arrival datasets
-crs_dep = data[['CRS_DEP_DATETIME_EST','CRS_DEP_DATE','CRS_DEP_HOUR','Marketing_Airline_Network','Origin','OriginCityName','Dest','DestCityName','CANCELLED']]
-crs_arr = data[['CRS_ARR_DATETIME_EST','CRS_ARR_DATE','CRS_ARR_HOUR','Marketing_Airline_Network','Origin','OriginCityName','Dest','DestCityName','CANCELLED']]
+crs_dep = data[['CRS_DEP_DATETIME_EST','CRS_DEP_DATE','CRS_DEP_HOUR','Marketing_Airline_Network','Origin','OriginCityName','Dest','DestCityName','CANCELLED','ArrDelayMinutes']]
+crs_arr = data[['CRS_ARR_DATETIME_EST','CRS_ARR_DATE','CRS_ARR_HOUR','Marketing_Airline_Network','Origin','OriginCityName','Dest','DestCityName','CANCELLED','ArrDelayMinutes']]
 
 #makes csv of each feature
 crs_dep.to_csv('crs_dep_cancelled.csv')
@@ -154,12 +180,8 @@ crs_arr.drop(crs_arr[crs_arr['CRS_ARR_DATE'] == '2017-12-31'].index, inplace=Tru
 crs_arr.drop(crs_arr[crs_arr['CRS_ARR_DATE'] == '2020-03-01'].index, inplace=True)
 
 
-dep_cancelled = crs_dep.groupby(['Marketing_Airline_Network','CRS_DEP_DATE','CRS_DEP_HOUR'])['CANCELLED'].sum().unstack(2).stack()  # ARR_DELAY_NEW is arrival delays that are positive
-arr_cancelled = crs_arr.groupby(['Marketing_Airline_Network','CRS_ARR_DATE','CRS_ARR_HOUR'])['CANCELLED'].sum().unstack(2).stack()  # ARR_DELAY_NEW is arrival delays that are positive
-
-# cumulative count of cancelled flights
-dep_cancelled = crs_dep.groupby(['Marketing_Airline_Network','CRS_DEP_DATE','CRS_DEP_HOUR'])['CANCELLED'].sum().unstack(2).stack().groupby(level=[0,1]).cumsum()  # ARR_DELAY_NEW is arrival delays that are positive
-arr_cancelled = crs_arr.groupby(['Marketing_Airline_Network','CRS_ARR_DATE','CRS_ARR_HOUR'])['CANCELLED'].sum().unstack(2).stack().groupby(level=[0,1]).cumsum()  # ARR_DELAY_NEW is arrival delays that are positive
+dep_cancelled = crs_dep.groupby(['Marketing_Airline_Network','CRS_DEP_DATE','CRS_DEP_HOUR'])['ArrDelayMinutes'].sum().unstack(2).stack()  # ARR_DELAY_NEW is arrival delays that are positive
+arr_cancelled = crs_arr.groupby(['Marketing_Airline_Network','CRS_ARR_DATE','CRS_ARR_HOUR'])['ArrDelayMinutes'].sum().unstack(2).stack()  # ARR_DELAY_NEW is arrival delays that are positive
 
 # sort based on date and hour:
 dep_cancelled = dep_cancelled.sort_index(level=(0,1))
@@ -172,16 +194,38 @@ final_df = pd.merge(arr_cancelled, dep_cancelled,  how='left', left_on=['Marketi
 final_df = final_df[['Marketing_Airline_Network','CRS_ARR_DATE','CRS_ARR_HOUR','0_x','0_y']]
 
 final_df.columns = ['Marketing_Airline_Network','CRS_ARR_DATE','CRS_ARR_HOUR','ArrDelayCANCELLED','DepDelayCANCELLED']
+
+
+# cumulative count of cancelled flights
+cumulative_dep_cancelled = crs_dep.groupby(['Marketing_Airline_Network','CRS_DEP_DATE','CRS_DEP_HOUR'])['ArrDelayMinutes'].sum().unstack(2).stack().groupby(level=[0,1]).cumsum()  # ARR_DELAY_NEW is arrival delays that are positive
+cumulative_arr_cancelled = crs_arr.groupby(['Marketing_Airline_Network','CRS_ARR_DATE','CRS_ARR_HOUR'])['ArrDelayMinutes'].sum().unstack(2).stack().groupby(level=[0,1]).cumsum()  # ARR_DELAY_NEW is arrival delays that are positive
+
+# sort based on date and hour:
+cumulative_dep_cancelled = cumulative_dep_cancelled.sort_index(level=(0,1))
+cumulative_arr_cancelled = cumulative_arr_cancelled.sort_index(level=(0,1))
+cumulative_dep_cancelled = cumulative_dep_cancelled.reset_index()
+cumulative_arr_cancelled = cumulative_arr_cancelled.reset_index()
+
+# Merge datasets together
+cumulative_final_df = pd.merge(cumulative_arr_cancelled, cumulative_dep_cancelled,  how='left', left_on=['Marketing_Airline_Network','CRS_ARR_DATE','CRS_ARR_HOUR'], right_on = ['Marketing_Airline_Network','CRS_DEP_DATE','CRS_DEP_HOUR'])
+cumulative_final_df = cumulative_final_df[['Marketing_Airline_Network','CRS_ARR_DATE','CRS_ARR_HOUR','0_x','0_y']]
+
+cumulative_final_df.columns = ['Marketing_Airline_Network','CRS_ARR_DATE','CRS_ARR_HOUR','CumulativeArrDelayCANCELLED','CumulativeDepDelayCANCELLED']
+cumulative_final_df = cumulative_final_df.fillna(method='ffill')
+
+final_df_2 = pd.merge(final_df, cumulative_final_df,  how='left', left_on=['Marketing_Airline_Network','CRS_ARR_DATE','CRS_ARR_HOUR'], right_on = ['Marketing_Airline_Network','CRS_ARR_DATE','CRS_ARR_HOUR'])
+final_df_2.columns
+
+
 # Fill in NAs using the same methodology
-final_df = final_df.fillna(method='ffill')
-final_df['CRS_ARR_DATE'] = pd.to_datetime(final_df['CRS_ARR_DATE'])
-final_df['Weekday'] = final_df['CRS_ARR_DATE'].dt.dayofweek # Add weekday variable
-final_df.to_csv('final_data_cancelled.csv')
+final_df_2['CRS_ARR_DATE'] = pd.to_datetime(final_df_2['CRS_ARR_DATE'])
+final_df_2['Weekday'] = final_df_2['CRS_ARR_DATE'].dt.dayofweek # Add weekday variable
+final_df_2['DepDelayCANCELLED'] = round(final_df_2['DepDelayCANCELLED'],0)
+final_df_2['ArrDelayCANCELLED'] = round(final_df_2['ArrDelayCANCELLED'],0)
+final_df_2.to_csv('daily_&_cumulative_final_data_cancelled.csv')
 
-final_df_WN = final_df[final_df['Marketing_Airline_Network']=='WN']
-final_df_WN.to_csv('final_data_cancelled_WN.csv')
-
-
+final_df_WN = final_df_2[final_df_2['Marketing_Airline_Network']=='WN']
+final_df_WN.to_csv('daily_&_cumulative_final_data_cancelled_WN.csv')
 
 
 
